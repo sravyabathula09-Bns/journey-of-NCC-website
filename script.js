cc2417c9fb66d2688d428cf1d1a7877498bd5196
BATHULA NAGA SRAVYA <sravyabathula09@gmail.com>
2026-03-06 00:03:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Runs when the webpage fully loads
+window.onload = function(){
+
+// Shows welcome popup message
+alert("Welcome to My first web page of NCC journey");
+
+}
+
+// Function to scroll to About section
+function scrollContent(){
+
+document.getElementById("about").scrollIntoView({
+behavior:"smooth"  // smooth scrolling effect
+});
+
+}

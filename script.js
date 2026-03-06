d130426fbfe458e03d470f26ec2b73aacbfd794d
sravyabathula09@gmail.com <sravyabathula09@gmail.com>
2026-03-06 11:57:07 +0530

    update javascript

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 // Runs when the webpage fully loads
 window.onload = function(){
 
 // Shows welcome popup message
 alert("Welcome to My first web page of NCC journey");
+this.alert("javascript is weoking");
 
 }
 
@@ -14,21 +14,4 @@ document.getElementById("about").scrollIntoView({
 behavior:"smooth"  // smooth scrolling effect
 });
 
-=======
-// Runs when the webpage fully loads
-window.onload = function(){
-
-// Shows welcome popup message
-alert("Welcome to My first web page of NCC journey");
-
-}
-
-// Function to scroll to About section
-function scrollContent(){
-
-document.getElementById("about").scrollIntoView({
-behavior:"smooth"  // smooth scrolling effect
-});
-
->>>>>>> cc2417c9fb66d2688d428cf1d1a7877498bd5196
-}
+}``

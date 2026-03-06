b78127346bb5e4dc33465e09e07a7f114928fff0
sravyabathula09@gmail.com <sravyabathula09@gmail.com>
2026-03-06 12:50:08 +0530

    fixed html css and js and updated styles

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// Runs when the webpage fully loads
 window.onload = function(){
-
-// Shows welcome popup message
-alert("Welcome to My first web page of NCC journey");
-this.alert("javascript is weoking");
+    
+alert("Welcome to My first web page of NCC Journey click ok to explore");
 
 }
 
 // Function to scroll to About section
+
 function scrollContent(){
 
 document.getElementById("about").scrollIntoView({
 behavior:"smooth"  // smooth scrolling effect
 });
 
-}``
+}
